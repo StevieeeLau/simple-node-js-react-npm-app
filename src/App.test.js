bbe045bafba5a2d21d16a3c8d330ab2759de8100
Steven <a2lstevenlau@gmail.com>
2024-06-23 22:23:04 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
-import React, {StrictMode} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.createRoot(div).render(
-    <StrictMode>
+  const root = createRoot(div);
+  root.render(
+    <React.StrictMode>
       <App />
-    </StrictMode>
+    </React.StrictMode>
   );
 });
